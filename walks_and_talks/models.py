c894293c324ae18d7fd7db2d9d5e1734eaecc38a
dariuszporebski79 <dariusz.porebski79@wp.pl>
2023-09-07 13:23:31 +0200

    creating models 1

--- walks_and_talks/models.py
+++ walks_and_talks/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class
+class DemocracyPositiveOpinions(models.Model):
+    opinion = models.TextField(unique=True)
+    comment = models.TextField()
+
+
+class DemocracyNegativeOpinions(models.Model):
+    opinion = models.TextField(unique=True)
+    comment = models.TextField()
+
+
+class ElectoralCommittee(models.Model):
+    committee_name = models.CharField(max_length=255, unique=True)
+    is_coalition = models.BooleanField()
+
+
+class Constituencies(models.Model):
+    number = models.SmallIntegerField(unique=True)
+    seat_of_commission = models.CharField(max_length=50)
+    description = models.TextField()
+    number_of_mandates = models.SmallIntegerField()
+    number_of_voters = models.IntegerField()
+
+# class Room(models.Model):
+#     room_name = models.CharField(max_length=255, unique=True)
+#     room_capacity = models.SmallIntegerField()
+#     projector_availability = models.BooleanField()
+#
+#
+# class RoomReservation(models.Model):
+#     room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+#     date_of_reservation = models.DateField()
+#     comment = models.TextField(default="No comment")
